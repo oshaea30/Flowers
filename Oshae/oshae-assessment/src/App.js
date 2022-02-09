ce63166c3bdb48ab11b5a3c6c00f6baf5b2957cb
Mike Alexis <terongp3@gmail.com>
2022-02-09 11:38:50 -0500

    First commit

---
+++ Oshae/oshae-assessment/src/App.js
@@ -0,0 +1,62 @@
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
+
+  const handleSearch = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = allData.filter((data) => {
+      return data.title.search(value) != -1;
+    });
+    setFilteredData(result);
+  };
+
+  useEffect(() => {
+    axios("http://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response.data);
+        setAllData(response.dtat);
+        setFilteredData(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting fake data:" + error);
+      });
+  }, []);
+
+  const styles = {
+    display: "inline",
+    width: "30%",
+    height: 50,
+    float: "left",
+    padding: 5,
+    border: "0.5px solid black",
+    marginBottom: 10,
+    marginRight: 10,
+  };
+
+  return (
+    <div className="App">
+      <div style={{ margin: "0 auto", marginTop: "10%" }}>
+        <label>Search:</label>
+        <input type="text" onChange={(event) => handleSearch(event)} />
+      </div>
+      <div style={{ padding: 10 }}>
+        {filteredData.map((value, index) => {
+          return (
+            <div  key={value.id}>
+              <div style={styles}>{value.title}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
